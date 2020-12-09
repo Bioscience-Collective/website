ee6762f382c9ea6dde39d0694a7812a4301ab0f9
Claire Wang <clairebookworm@Claires-Batcave.lan1>
2020-12-09 13:36:49 -0800

    text

--- components/PageSpec/Index/Content.jsx
+++ components/PageSpec/Index/Content.jsx
@@ -2,14 +2,11 @@ export default function IndexContent() {
   return (
     <div className="text-2xl py-5 space-y-10">
       <p>
-        As hackathon organizers, we are doing all the "dirty work" to make
-        events happen. And now more than ever, we need better tools and
-        platforms to make organizing easy and efficient, even when we are
-        remote.
+      We provide free and comprehensive resources to promote interest and knowledge in biology.
+Here are some of our current projects.
       </p>
       <p className="font-bold">
-        Join us to build the tools that you wish you had for the next generation
-        of hackathon organizers.
+        Join us to build the tools that you wish you had for the next generation of groundbreakers.
       </p>
     </div>
   );
